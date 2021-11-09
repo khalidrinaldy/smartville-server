c5aaf6ff7fe1f92c22248021e6e02e28bc991986
khalidrinaldy <alid.rinaldy@gmail.com>
2021-11-09 11:03:30 +0700

    test

--- db/connect.db.go
+++ db/connect.db.go
@@ -1,23 +1,28 @@
 package db
 
 import (
-	// "fmt"
-	// "smartville-server/config"
+	"fmt"
 	"os"
-
+	//"smartville-server/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func OpenDatabase() *gorm.DB {
-	// cfg, _ := config.NewConfig(".env")
+	//cfg, _ := config.NewConfig(".env")
 	// dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
 	// 	cfg.Database.Host,
 	// 	cfg.Database.Port,
 	// 	cfg.Database.Username,
 	// 	cfg.Database.Password,
 	// 	cfg.Database.Name)
-	db, err := gorm.Open(postgres.Open(string(os.Getenv("DATABASE_URL"))), &gorm.Config{})
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_DATABASE"))
+	db, err := gorm.Open(postgres.Open(string(dsn)), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
