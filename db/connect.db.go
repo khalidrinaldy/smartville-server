d06c45ca8360e1d04ff43501c9862b8629b8cd8d
khalidrinaldy <alid.rinaldy@gmail.com>
2021-11-09 11:20:49 +0700

    test

--- db/connect.db.go
+++ db/connect.db.go
@@ -16,12 +16,14 @@ func OpenDatabase() *gorm.DB {
 	// 	cfg.Database.Username,
 	// 	cfg.Database.Password,
 	// 	cfg.Database.Name)
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s database=%s sslmode=require",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
 		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_DATABASE"))
+		os.Getenv("DB_DATABASE"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("SSL_MODE"),
+		os.Getenv("TIMEZONE"))
 	db, err := gorm.Open(postgres.Open(string(dsn)), &gorm.Config{})
 	if err != nil {
 		panic(err)
