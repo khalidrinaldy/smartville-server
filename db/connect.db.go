96f1dc0734834920dad2ac7ab4da4fc1f6852393
khalidrinaldy <alid.rinaldy@gmail.com>
2021-11-09 11:10:50 +0700

    test

--- db/connect.db.go
+++ db/connect.db.go
@@ -16,7 +16,7 @@ func OpenDatabase() *gorm.DB {
 	// 	cfg.Database.Username,
 	// 	cfg.Database.Password,
 	// 	cfg.Database.Name)
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_USER"),
