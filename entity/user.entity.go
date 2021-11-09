f6830a5fb5068745043226c969e36837af745d2e
khalidrinaldy <alid.rinaldy@gmail.com>
2021-11-09 13:27:08 +0700

    test

--- entity/user.entity.go
+++ entity/user.entity.go
@@ -2,23 +2,42 @@ package entity
 
 import "time"
 
+// type User struct {
+// 	Id            int64  `gorm:"primaryKey;not null;autoIncrement"`
+// 	Nik           string `gorm:"type:varchar(16);primaryKey;not null"`
+// 	Nama          string `gorm:"type:varchar(100);not null"`
+// 	Email         string `gorm:"type:varchar(100);not null"`
+// 	Password      string `gorm:"type:varchar(255);not null"`
+// 	Tgl_lahir     time.Time
+// 	Tempat_lahir  string `gorm:"type:varchar(50);not null"`
+// 	Alamat        string `gorm:"type:varchar(255);not null"`
+// 	Dusun         string `gorm:"type:varchar(50);not null"`
+// 	Rt            int    `gorm:"type:int(2);not null"`
+// 	Rw            int    `gorm:"type:int(2);not null"`
+// 	Jenis_kelamin bool   `gorm:"not null"`
+// 	No_hp         string `gorm:"type:varchar(15);not null"`
+// 	Role          string `gorm:"type:varchar(15);default:user"`
+// 	Profile_pic   string `gorm:"type:varchar(300);not null"`
+// 	Token         string `gorm:"type:varchar(255);not null"`
+// }
+
 type User struct {
 	Id            int64  `gorm:"primaryKey;not null;autoIncrement"`
-	Nik           string `gorm:"type:varchar(16);primaryKey;not null"`
-	Nama          string `gorm:"type:varchar(100);not null"`
-	Email         string `gorm:"type:varchar(100);not null"`
-	Password      string `gorm:"type:varchar(255);not null"`
+	Nik           string `gorm:"primaryKey;not null"`
+	Nama          string `gorm:"not null"`
+	Email         string `gorm:"not null"`
+	Password      string `gorm:"not null"`
 	Tgl_lahir     time.Time
-	Tempat_lahir  string `gorm:"type:varchar(50);not null"`
-	Alamat        string `gorm:"type:varchar(255);not null"`
-	Dusun         string `gorm:"type:varchar(50);not null"`
-	Rt            int    `gorm:"type:int(2);not null"`
-	Rw            int    `gorm:"type:int(2);not null"`
+	Tempat_lahir  string `gorm:"not null"`
+	Alamat        string `gorm:"not null"`
+	Dusun         string `gorm:"not null"`
+	Rt            int    `gorm:"not null"`
+	Rw            int    `gorm:"not null"`
 	Jenis_kelamin bool   `gorm:"not null"`
-	No_hp         string `gorm:"type:varchar(15);not null"`
-	Role          string `gorm:"type:varchar(15);default:user"`
-	Profile_pic   string `gorm:"type:varchar(300);not null"`
-	Token         string `gorm:"type:varchar(255);not null"`
+	No_hp         string `gorm:"not null"`
+	Role          string `gorm:"default:user"`
+	Profile_pic   string `gorm:"not null"`
+	Token         string `gorm:"not null"`
 }
 
 type UserList struct {
