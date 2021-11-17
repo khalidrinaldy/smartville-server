b2254a43f41d11a27997996c9dfbfbe82ee57137
khalidrinaldy <alid.rinaldy@gmail.com>
2021-11-17 14:36:04 +0700

    upload image finished

--- routes/routes.go
+++ routes/routes.go
@@ -1,17 +1,11 @@
 package routes
 
 import (
-	"context"
-	"io"
 	"net/http"
-	"os"
 	"path"
-	"smartville-server/config"
 	"smartville-server/db"
 	"smartville-server/middleware"
 	"smartville-server/repository"
-	"github.com/cloudinary/cloudinary-go/api/uploader"
-
 	"github.com/labstack/echo"
 )
 
@@ -42,58 +36,4 @@ func InitRoute(ech *echo.Echo) {
 
 	//Change password
 	ech.POST("/user/change-password", repository.ChangePassword(database))
-
-	//Test Upload image
-	ech.POST("/image", func(c echo.Context) error {
-		cld, err := config.CloudConfig()
-		if err != nil {
-			return c.JSON(http.StatusOK, map[string]interface{}{
-				"message": "Error occured cloud config",
-				"data":    err,
-			})
-		}
-		// Source
-		file, err := c.FormFile("image")
-		if err != nil {
-			return err
-		}
-		src, err := file.Open()
-		if err != nil {
-			return err
-		}
-		defer src.Close()
-
-		// Destination
-		dst, err := os.Create(file.Filename)
-		if err != nil {
-			return err
-		}
-		defer dst.Close()
-
-		// Copy
-		if _, err = io.Copy(dst, src); err != nil {
-			return err
-		}
-
-		uploadResult, err := cld.Upload.Upload(
-			context.Background(),
-			file.Filename,
-			uploader.UploadParams{
-				Folder: "test/test2",
-				UseFilename: true,
-				//DiscardOriginalFilename: true,
-				FilenameOverride: "Asuna",
-			},
-		)
-		if err != nil {
-			return c.JSON(http.StatusOK, map[string]interface{}{
-				"message": "Error occured upload image",
-				"data":    err.Error(),
-			})
-		}
-		return c.JSON(http.StatusOK, map[string]interface{}{
-			"message": "Upload success",
-			"data":    uploadResult.URL,
-		})
-	})
 }
