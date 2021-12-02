f2dce35ea802716707f8d2de20ddf5c5fd5ca26e
khalidrinaldy <alid.rinaldy@gmail.com>
2021-12-02 11:14:58 +0700

    test error

--- helper/notification.helper.go
+++ helper/notification.helper.go
@@ -12,7 +12,7 @@ import (
 
 func SendNotification(registrationToken string, notificationMsg string) (string,error) {
 	//Initialize App
-	opt := option.WithCredentialsFile("firebase-key.json")
+	opt := option.WithCredentialsFile("")
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		return "error initializing app", err
